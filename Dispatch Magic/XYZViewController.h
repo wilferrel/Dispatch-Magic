d682b6701d38bcb614deb7306af67d92af9938a7
Justin Ison <justin@taximagic.com>
2013-09-04 14:17:24 -0400

    Adding DIspatch Magic to github

--- Dispatch Magic/XYZViewController.h
+++ Dispatch Magic/XYZViewController.h
@@ -7,13 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#define environments 0
+#define events 1
+#define locations 2
 
 @interface SNAViewController : UIViewController
 <UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate>
 
-@property (strong, nonatomic) IBOutlet UIPickerView *envPicker;
+@property (strong, nonatomic) IBOutlet UIPickerView *picker;
 
-@property (strong, nonatomic) NSArray* environments;
+@property (strong, nonatomic) NSArray* _environments;
+
+@property (strong, nonatomic) NSArray* _events;
+
+@property (strong, nonatomic) NSArray* _locations;
+
+@property (strong, nonatomic) id latitude;
+@property (strong, nonatomic) id longitude;
 
 @property (strong, nonatomic) IBOutlet UITextField *rideIDTextField;
 
--- Dispatch Magic/XYZViewController.m
+++ Dispatch Magic/XYZViewController.m
@@ -18,25 +18,20 @@
 
 @implementation SNAViewController
 
-//@synthesize envPicker;
-//@synthesize environments;
-//@synthesize rideIDTextField;
-
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    
-    //Load the NSArray Object
    
-    self.environments = @[@"trunk", @"release"];
+    self._environments = @[@"trunk", @"release"];
+    self._events = @[@"assign", @"on_site", @"meter_on", @"meter_off", @"provider_cancel", @"gps"];
+    self._locations = @[@"DC", @"DCA", @"HQ", @"JFK", @"LA", @"LAX", @"LHR", @"LON", @"NYC", @"SF", @"SFO"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
@@ -46,41 +41,77 @@
         return NO;
 }
 
-- (IBAction)assignAction:(UIButton *)sender {
-    NSLog(@"I PRESSED THE Assign BUTTON");
- 
-    
-//    // GET EXAMPLE
-//    NSString *urlAsString = @"http://api.openweathermap.org/data/2.5/weather?id=2172797";
-//    NSURL *url = [NSURL URLWithString:urlAsString];
-//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-//    
-//    AFJSONRequestOperation *operation = [AFJSONRequestOperation
-//        JSONRequestOperationWithRequest:request success:^(NSURLRequest *request,
-//        NSHTTPURLResponse *response, id JSON)
-//    {
-//        //success
-//        NSArray *info = [JSON valueForKeyPath:@"weather.description"];
-//        NSLog(@"data output: %@", info);
-//    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON)
-//    {
-//        //error
-//        NSLog(@"Error contacting server: %@", [error localizedDescription]);
-//    }];
-// [operation start];
-    
+//THIS METHOD CLOSES KEYBOARD WHEN TOUCHED OUTSIDE OF TEXT FIELD
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.rideIDTextField resignFirstResponder];
+}
 
+- (IBAction)assignAction:(UIButton *)sender {
+    NSLog(@"PRESSED THE SUBMIT BUTTON");
+ 
+    //SET PICKER LOCATION VARIABLES
+    int selectEnv = [self.picker selectedRowInComponent:0];
+    int selectEvent = [self.picker selectedRowInComponent:1];
+    int selectLocation = [self.picker selectedRowInComponent:2];
     
-    int selectEnv = [self.envPicker selectedRowInComponent:0];
+ 
+    //SET LAT AND LON FOR LOCATIONS IN PICKER
+    if ([self._locations[selectLocation] isEqual: @"HQ"]) {
+            self.latitude = @"38.79271";
+            self.longitude = @"-77.06002";
+    }
+    else if ([self._locations[selectLocation] isEqual: @"DC"]) {
+            self.latitude = @"38.906";
+            self.longitude = @"-77.036";
+    }
+    else if ([self._locations[selectLocation] isEqual: @"DCA"]) {
+        self.latitude = @"38.8496";
+        self.longitude = @"-77.0425";
+    }
+    else if ([self._locations[selectLocation] isEqual: @"JFK"]) {
+        self.latitude = @"40.6461";
+        self.longitude = @"-73.7821";
+    }
+    else if ([self._locations[selectLocation] isEqual: @"LA"]) {
+        self.latitude = @"34.105";
+        self.longitude = @"-118.25";
+    }
+    else if ([self._locations[selectLocation] isEqual: @"LAX"]) {
+        self.latitude = @"33.9446";
+        self.longitude = @"-118.4095";
+    }
+    else if ([self._locations[selectLocation] isEqual: @"LHR"]) {
+        self.latitude = @"51.4709";
+        self.longitude = @"-0.452";
+    }
+    else if ([self._locations[selectLocation] isEqual: @"LON"]) {
+        self.latitude = @"51.527";
+        self.longitude = @"-0.129";
+    }
+    else if ([self._locations[selectLocation] isEqual: @"NYC"]) {
+        self.latitude = @"40.70730";
+        self.longitude = @"-74.01103";
+    }
+    else if ([self._locations[selectLocation] isEqual: @"SF"]) {
+        self.latitude = @"37.7799";
+        self.longitude = @"-122.419";
+    }
+    else if ([self._locations[selectLocation] isEqual: @"SFO"]) {
+        self.latitude = @"37.6218";
+        self.longitude = @"-122.3810";
+    }
     
     
+    //RIDE EVENT POST
     AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:[NSURL URLWithString:@"http://www.trunk.ridecharge.com/"]];
     httpClient.requestSerializer = [AFJSONSerializer serializer];
     NSMutableURLRequest *request = [httpClient requestWithMethod:@"POST"
                                                        URLString:[NSString stringWithFormat:@"http://www.%@.ridecharge.com/services/fleet_events/ride_charge/fake/1/aaeeff11",
-                                                                  self.environments[selectEnv]
+                                                                  self._environments[selectEnv]
                                                                   ]
-                                                        parameters:@{@"event":@"assign", @"latitude":@"38.8525", @"longitude":@"-77.038", @"ride_id":self.rideIDTextField.text, @"driver_id":@"1", @"vehicle_number":@"222", @"driver_phone_number":@"5555555555", @"driver_name":@"Mr. iOS"}];
+                                                      parameters:@{@"event":self._events[selectEvent], @"latitude":self.latitude, @"longitude":self.longitude, @"ride_id":self.rideIDTextField.text, @"driver_id":@"1", @"vehicle_number":@"222", @"driver_phone_number":@"5555555555", @"driver_name":@"Mr. iOS"}
+                                    ];
    
     AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
         NSLog(@"App.net Global Stream: %@", JSON);
@@ -93,25 +124,6 @@
     
 }
 
-- (IBAction)reDispatchAction:(id)sender {
-    NSLog(@"I PRESSED THE Re-Dispatch BUTTON");
-}
-
-- (IBAction)onSiteAction:(id)sender {
-    NSLog(@"I PRESSED THE On Site BUTTON");
-}
-
-- (IBAction)meterOnAction:(id)sender {
-    NSLog(@"I PRESSED THE Meter On BUTTON");
-}
-
-- (IBAction)meterOffAction:(id)sender {
-    NSLog(@"I PRESSED THE Meter Off BUTTON");
-}
-
-- (IBAction)providerCancelAction:(id)sender {
-    NSLog(@"I PRESSED THE Meter Provider Cancel BUTTON");
-}
 
 - (IBAction)rideIDTextField:(id)sender {
     NSLog(@"Typing in the text field!!!");
@@ -123,17 +135,35 @@
 
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
 {
-    return 1;
+    return 3;
 }
 
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
-    return self.environments.count;
+    if (component == environments)
+        return self._environments.count;
+    
+    if (component == events)
+    return self._events.count;
+    
+    if (component == locations)
+        return self._locations.count;
+    
+    return 0;
 }
 
 - (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
 {
-    return [self.environments objectAtIndex:row];
+    if (component == environments)
+    return [self._environments objectAtIndex:row];
+    
+    if (component == events)
+    return [self._events objectAtIndex:row];
+    
+    if (component == locations)
+        return [self._locations objectAtIndex:row];
+    
+    return 0;
 }
 
 
